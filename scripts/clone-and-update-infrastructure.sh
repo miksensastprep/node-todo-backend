c77be2d89ade4b8ff7d358d7c1163dd0b8e67dbb
miksensastprep <36421519+miksensastprep@users.noreply.github.com>
2018-02-12 22:20:54 +0100

    Update clone-and-update-infrastructure.sh

--- scripts/clone-and-update-infrastructure.sh
+++ scripts/clone-and-update-infrastructure.sh
@@ -9,7 +9,7 @@ gitref=${1:-noref}
 version=${2:-latest}
 path=${3:-terraform-infrastructure-live/gce_account/europe-west1/fast/todo-backend-fast}
 infrarepo=${4:-`echo $CIRCLE_REPOSITORY_URL | awk -F'/' '{print $1}'`/terraform-infrastructure-live.git}
-
+githubaccount=${5:-`echo $CIRCLE_REPOSITORY_URL | awk -F'/' '{print $1}' | awk -F':' '{print $2}'`}
 # Add github ssh key
 if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
@@ -19,6 +19,7 @@ git clone ${infrarepo}
 cd ${path}
 echo version = ${version} > version.tfvars
 sed -i s/?ref=.*\"/?ref=${gitref}\"/ terraform.tfvars
+sed -i s/github.com\\/.*\\//github.com\\/${githubaccount}\\// terraform.tfvars
 
 git config --global user.email "mikael+circleci@sennerholm.net"
 git config --global user.name "Circle CI"
